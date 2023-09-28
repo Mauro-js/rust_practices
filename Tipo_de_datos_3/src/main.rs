edb507a84cc41efb60acf56aad5636d4a34e45da
MauroJs <niev.mauro@gmail.com>
2023-09-28 00:27:19 -0300

    varios tipos de datos

--- Tipo_de_datos_3/src/main.rs
+++ Tipo_de_datos_3/src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x:u32 = 5;
+
+    println!("The value of x is {}", x);
+
+    x = 3999999;
+
+    println!("The value of x is {}", x);
+
+    let suma = 58 + 67;
+
+    let resta = 34343.39 - 788.3;
+
+    let multiplicacion = 4455.3 * 23.0;
+
+    let division = 34 / 4;
+
+    let resto = 34 % 4;
+
+    let verdadero = true;
+    let falso: bool = false;
+
+    let a = "a";
+
+    let tupla:(f32,u8,f32) = (500.32, 25, -56.21);
+
+    let (y,z,l) = tupla;
+
+    let primero = tupla.0;
+
+    println!("el segundo valor de la tupla es: {}", z);
+
+
+    
 }
