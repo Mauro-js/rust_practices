8046c306e560d1901cb03fd8c3c25586faf213e1
MauroJs <niev.mauro@gmail.com>
2023-09-27 23:41:22 -0300

    nueva carpeta

--- Variables_2/src/main.rs
+++ Variables_2/src/main.rs
@@ -1,4 +1,17 @@
 fn main() {
-    let x = 3;
+    let mut x = 3;
     println!("El valor de x es: {}", x);
+
+    x = 7;
+
+    println!("El valor de x es: {}", x);
+
+    const PI: f32 = 3.1416;
+
+    println!("El valor de PI es: {}", PI);
+    
+    let y = 3;
+    let y = "a";
+
+    println!("Por shadowing el valor de y es: {}", y)
 }
